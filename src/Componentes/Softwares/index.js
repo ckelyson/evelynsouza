7b25a5684e7b950587f2bc372059b77055f7809f
Ckelyson Moura <ckelysonjheymison777@gmail.com>
2023-11-15 08:17:45 -0300

    atualizando oq deixei para trás

--- src/Componentes/Softwares/index.js
+++ src/Componentes/Softwares/index.js
@@ -58,8 +58,8 @@ function Softwares () {
                 </div>
 
                 <div className='ferramentas'>
-                    <h1>NORMAS REGULAMENTADORAS</h1>
-                    <h1>NORMAS DE HIGIENE OCUPACIONAL</h1>
+                    <h1>NORMAS REGULAMENTADORAS E<br/> NORMAS DE HIGIENE OCUPACIONAL</h1>
+                    
                     <div className='icone'>
                         <img src={pdca} className='pdca'/>
                         <p>PDCA</p>
